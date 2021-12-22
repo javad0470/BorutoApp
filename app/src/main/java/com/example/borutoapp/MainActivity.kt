7592d770cf10fb7cd412298c10d3de09a2c8d12e
Javad <javad0470@gmail.com>
2021-12-22 21:34:18 +0330

    setup navigation

--- app/src/main/java/com/example/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -8,14 +8,22 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.borutoapp.navigation.SetupNavGraph
 import com.example.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
+                navHostController = rememberNavController()
 
+                SetupNavGraph(navController = navHostController)
             }
         }
     }
---
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.borutoapp.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+
+    const val HERO_DATABASE_TABLE = "hero_table"
+
+
+}
